598f4670511e8aacc95b747666f4afb758a57e46
Mike Holdsworth <mike@monowai.com>
2013-07-03 19:36:14 +1200

    GPL

--- src/main/java/com/auditbucket/audit/dao/IAuditTagDao.java
+++ src/main/java/com/auditbucket/audit/dao/IAuditTagDao.java
@@ -1,3 +1,22 @@
+/*
+ * Copyright (c) 2012-2013 "Monowai Developments Limited"
+ *
+ * This file is part of AuditBucket.
+ *
+ * AuditBucket is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * AuditBucket is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with AuditBucket.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.auditbucket.audit.dao;
 
 import com.auditbucket.audit.model.IAuditHeader;
--- src/main/java/com/auditbucket/audit/repo/neo4j/model/AuditHeader.java
+++ src/main/java/com/auditbucket/audit/repo/neo4j/model/AuditHeader.java
@@ -1,10 +1,29 @@
+/*
+ * Copyright (c) 2012-2013 "Monowai Developments Limited"
+ *
+ * This file is part of AuditBucket.
+ *
+ * AuditBucket is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * AuditBucket is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with AuditBucket.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.auditbucket.audit.repo.neo4j.model;
+
 import com.auditbucket.audit.bean.AuditHeaderInputBean;
 import com.auditbucket.audit.model.IAuditHeader;
 import com.auditbucket.audit.model.IAuditWhen;
 import com.auditbucket.audit.model.IDocumentType;
 import com.auditbucket.audit.model.ITagValue;
-import com.auditbucket.audit.repo.neo4j.model.AuditTagValue;
-import com.auditbucket.audit.repo.neo4j.model.AuditWhen;
 import com.auditbucket.registration.model.IFortress;
 import com.auditbucket.registration.model.IFortressUser;
 import com.auditbucket.registration.repo.neo4j.model.Fortress;
@@ -22,10 +41,6 @@ import java.util.HashSet;
 import java.util.Set;
 import java.util.UUID;
 
-/**
- * copyright
- */
-package com.auditbucket.audit.repo.neo4j.model;
 
 /**
  * User: mike
