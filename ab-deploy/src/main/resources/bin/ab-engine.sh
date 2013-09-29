a102b4d107e4b28b28abaf3935ec8267ec1a4258
Mike Holdsworth <mike@monowai.com>
2013-09-29 15:02:35 +1300

    Start/Stop scripts

--- ab-deploy/src/main/resources/bin/ab-engine.sh
+++ ab-deploy/src/main/resources/bin/ab-engine.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-FRIENDLY_NAME="AuditBucket Engine Server"
+FRIENDLY_NAME="AuditBucket Engine Service"
 LAUNCHD_NAME="com.auditbucket.engine"
+TIMEOUT=120
 
 function findBaseDirAndCdThere {
     # This seems to not be safe to run at any time. If that
@@ -28,6 +29,9 @@ function findBaseDirAndCdThere {
   AB_LOG=$AB_INSTANCE/data/log
 
   cd "$AB_HOME"
+  if [ ! -d "$AB_HOME/data" ]; then
+      mkdir "$AB_HOME/data"
+  fi
 }
 
 function parseConfig {
@@ -54,10 +58,16 @@ buildclasspath() {
 }
 
 showinfo() {
-  reportstatus
-  buildclasspath
 
-  echo "ABE_HOME:        $AB_HOME"
+  buildclasspath
+  detectrunning
+  if [ $newpid ] ; then
+      echo "$FRIENDLY_NAME is running on $newpid"
+   else
+      echo "$FRIENDLY_NAME is not running"
+  fi
+
+  echo "AB_HOME:         $AB_HOME"
   echo "ABE_INTEGRATION: $AB_INTEGRATION"
   echo "ABE_NEO4J:       $ABE_NEO4J"
   echo "ABE_HTTP_PORT:   $ABE_HTTP_PORT"
@@ -66,6 +76,22 @@ showinfo() {
   echo "JAVA_OPTS:       $JAVA_OPTS"
   echo "CLASSPATH:       $CLASSPATH"
 }
+
+checkstatus() {
+
+  if [ -e "$PID_FILE" ] ; then
+    ABE_PID=$( cat "$PID_FILE" )
+    kill -0 $ABE_PID 2>/dev/null || ABE_PID=
+  fi
+}
+
+detectrunning() {
+  ## This could be achieved with filtering using -sTCP:LISTEN but this option is not available
+  ## on lsof v4.78 which is the one bundled with some distros. So we have to do this grep below
+  newpid=$(lsof -i :$ABE_HTTP_PORT -F T -Ts | grep -i "TST=LISTEN" -B1 | head -n1)
+  newpid=${newpid:1}
+}
+
 
 
 if [ -x "$JAVA_HOME/bin/java" ]; then
@@ -83,28 +109,155 @@ buildclasspath
 source bin/utils
 parseConfig
 
-ABE_HTTP_PORT="${wrapper_httpPort:=9092}"
-AB_INTEGRATION="${wrapper_ab_integration:=http}"
-ABE_NEO4J=${wrapper_neo4j}
-
-JAVA_OPTS="headless=true -XX:+DisableExplicitGC ${wrapper_java_additional} --debug"
-
 [ -z "${wrapper_java_initmemory}" ] || JAVA_OPTS="$JAVA_OPTS -Xms${wrapper_java_initmemory}m"
 [ -z "${wrapper_java_maxmemory}" ] || JAVA_OPTS="$JAVA_OPTS -Xmx${wrapper_java_maxmemory}m"
 
-showinfo
+ABE_HTTP_PORT="${wrapper_httpPort:=9092}"
+AB_INTEGRATION="${wrapper_ab_integration:=http}"
+ABE_NEO4J=${wrapper_neo4j}
+PID_FILE=$AB_HOME/data/abe.pid
 
-$JAVA -cp '$CLASSPATH' -jar $AB_APP/ab-engine-1.0.0-BUILD-SNAPSHOT-war-exec.jar $JAVA_OPTS  \
-        -Djava.awt.headless=true \
-        -Dneo4j=${ABE_NEO4J} \
-        -httpPort=${ABE_HTTP_PORT} \
-        -Dab.integration=${AB_INTEGRATION}  \
-        -Dab.config=${AB_CONFIG}/config.properties  \
-        -extractDirectory=".extracts/engine"\
-        -DAB_HOME=${AB_HOME} \
-        -Dlog4j.configuration=file://${AB_CONFIG}/log4j-engine.xml
-        #-Djava.util.logging.config.file=file://${AB_CONFIG}/logging.properties
+JAVA_OPTS="headless=true -XX:+DisableExplicitGC ${wrapper_java_additional}"
+
+startit() {
+    echo "looking to start"
+    detectos
+    #showinfo
+     if [ $DIST_OS = "macosx" ] ; then
+        getlaunchdpid
+        if [ $LAUNCHDPID -eq 0 ] ; then
+          echo "Detected installation in launchd, starting it..."
+          launchctl start $LAUNCHD_NAME
+          exit 0
+        elif [ $LAUNCHDPID -gt 0 ] ; then
+          echo "Instance already running via launchd with PID $LAUNCHDPID"
+          exit 1
+        fi
+        # We fall through here since if there is no launchd install we start manually
+      fi
+
+        detectrunning
+        if [ $newpid ] ; then
+	         echo "Another server-process is running with [$newpid], cannot start a new one. Exiting."
+	        exit 2;
+        fi
+
+      checkstatus
+
+      if [ -z $ABE_PID ] ; then
+        printf "Starting $FRIENDLY_NAME..."
+
+        if [ -e "$PID_FILE" ] ; then
+            echo "removing stale PID file"
+            rm $PID_FILE
+        fi
+
+        $JAVA -cp '$CLASSPATH' -jar $AB_APP/ab-engine-1.0.0-BUILD-SNAPSHOT-war-exec.jar $JAVA_OPTS  \
+            -Djava.awt.headless=true \
+            -Dneo4j=${ABE_NEO4J} \
+            -httpPort=${ABE_HTTP_PORT} \
+            -Dab.integration=${AB_INTEGRATION}  \
+            -Dab.config=${AB_CONFIG}/config.properties  \
+            -extractDirectory=".extracts/engine" \
+            -DAB_HOME=${AB_HOME} \
+            -Dlog4j.configuration=file://${AB_CONFIG}/log4j-engine.xml& > "$AB_HOME/log/abe-console.txt" & ps aux |grep [h]ttpPort=${ABE_HTTP_PORT} | awk '/[a]b-engine/{print $2}' >> $PID_FILE
+
+        disown
+
+      else
+          echo "$FRIENDLY_NAME already running with pid $ABE_PID"
+          exit 0
+      fi
+}
+
+stopit() {
+
+  detectos
+  checkwriteaccess
+
+  if [ $DIST_OS = "macosx" ] ; then
+    getlaunchdpid
+    if [ $LAUNCHDPID -gt 0 ] ; then
+      echo "Instance running via launchd with PID $LAUNCHDPID, stopping it..."
+      launchctl stop $LAUNCHD_NAME
+      return
+    fi
+    # We fall through here since if there is no launchd install we stop manually started instance
+  fi
+
+  ABE_PID=$( cat "$PID_FILE" )
+
+  if [ -z $ABE_PID ] ; then
+    echo "ERROR: $FRIENDLY_NAME not running"
+    [ -e "$PID_FILE" ] && rm "$PID_FILE"
+  else
+    printf "Stopping $FRIENDLY_NAME [$ABE_PID]..."
+    x=0
+    while [ $x -lt $TIMEOUT ] && [ "$ABE_PID" != "" ]  ; do
+      kill $ABE_PID 2>/dev/null
+      printf "."
+      sleep 1
+      detectrunning
+      if [ !"$newpid" ] ; then
+        echo "Stopped"
+        break
+      fi
+      x=$[$x+1]
+    done
+	  [  $x -eq $TIMEOUT ] && ( echo " force shutdown" ;  kill -9 $ABE_PID>/dev/null ) || echo " done"
+	  [ -e "$PID_FILE" ] && rm  "$PID_FILE"
+  fi
+}
 
 
+case "${!OPTIND}" in
+  console)
+    console
+    exit 0
+    ;;
 
-#java -jar ab-engine-1.0.0-BUILD-SNAPSHOT-war-exec.jar -Dab.integration=http -httpPort=8090 -Dlog4j.configuration=./log4j.xml -Dab.config=./config.properties
+  start)
+    WAIT=true
+    startit
+    ;;
+
+  start-no-wait)
+    WAIT=false
+    startit
+    exit 0
+    ;;
+
+  stop)
+    stopit
+    exit 0
+    ;;
+
+  restart)
+    WAIT=true
+    stopit
+    startit
+    exit 0
+    ;;
+
+  status)
+    reportstatus
+    exit 0
+    ;;
+
+  info)
+    showinfo
+    exit 0
+    ;;
+  install)
+    WAIT=true
+    installservice
+    exit 0
+    ;;
+  remove)
+    removeservice
+    exit 0
+    ;;
+  *)
+    echo "Usage: ab-engine { console | start | start-no-wait | stop | restart | status | info | install | remove }"
+    exit 0;;
+esac
