f364590afbbd0b50269b73bb747f61365889951d
Nabil Belakbir <nabilblk@gmail.com>
2014-05-10 13:54:21 +0100

    DAT-25 : Spring integration Ping Ab-Engine -> Ab-Search

---
+++ ab-engine/src/main/java/com/auditbucket/engine/service/AbMonitoringGateway.java
@@ -0,0 +1,16 @@
+package com.auditbucket.engine.service;
+
+import com.auditbucket.search.model.PingResult;
+import org.springframework.integration.annotation.Gateway;
+import org.springframework.integration.annotation.Payload;
+
+public interface AbMonitoringGateway {
+    /**
+     * A required Payload must be declared due to spring Integration constraint
+     * http://docs.spring.io/spring-integration/reference/html/messaging-endpoints-chapter.html#gateway-calling-no-argument-methods
+     * @return PingResult
+     */
+    @Payload("new java.util.Date()")
+    @Gateway(requestChannel = "pingEsRequest", replyChannel = "pingEsReply")
+    public PingResult ping();
+}
