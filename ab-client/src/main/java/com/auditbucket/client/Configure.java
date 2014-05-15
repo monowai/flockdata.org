189b66ae7bcaf360a8fdd6b12882aba3648af7ba
Mike Holdsworth <mike@monowai.com>
2014-05-15 14:44:53 +1200

    DAT-60 Improve logging. Remove password as a command line option

--- ab-client/src/main/java/com/auditbucket/client/Configure.java
+++ ab-client/src/main/java/com/auditbucket/client/Configure.java
@@ -1,14 +1,16 @@
 package com.auditbucket.client;
 
-import com.auditbucket.helper.DatagioException;
 import com.auditbucket.registration.bean.SystemUserResultBean;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import net.sourceforge.argparse4j.ArgumentParsers;
 import net.sourceforge.argparse4j.impl.Arguments;
 import net.sourceforge.argparse4j.inf.ArgumentParser;
 import net.sourceforge.argparse4j.inf.ArgumentParserException;
 import net.sourceforge.argparse4j.inf.MutuallyExclusiveGroup;
 import net.sourceforge.argparse4j.inf.Namespace;
+import org.apache.log4j.ConsoleAppender;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PatternLayout;
 import org.slf4j.LoggerFactory;
 
 import java.io.*;
@@ -21,32 +23,33 @@ import java.util.Properties;
  * Time: 12:42 PM
  */
 public class Configure {
-    private static org.slf4j.Logger logger = LoggerFactory.getLogger(Configure.class);
+    private static org.slf4j.Logger logger;
     static String configFile = "client.config";
     static String internalUser = null;
     static String internalPass = null;
 
     public static void main(String args[]) {
 
-
         Namespace ns = getCommandLineArgs(args);
-        internalPass = ns.getString("pass");
         internalUser = ns.getString("user");
 
         File file = getFile(configFile, ns);
         ConfigProperties defaults = readConfiguration(file);
+        configureLogger(ns.getBoolean("debug"));
 
         boolean reconfigure = ns.getBoolean("reconfig");
 
         if (!file.exists() || reconfigure) {
-            if ( internalUser == null ){
-                logger.error("No admin user supplied to register accounts. Please call configure -u=user -p=pass");
-
-                System.exit (-2);
+            while (internalUser == null) {
+                //logger.error("No admin user supplied to register accounts. Please call [configure -u=user]");
+                // System.exit(-2);
+                internalUser = getValue("Admin account (admin): ", "** Enter the admin account to use", "admin");
             }
-            logger.info("** Starting default configuration process");
+
+            logger.info("** Starting {}configuration process", (reconfigure?"re": "initial "));
             String engineURL = defaults.getEngineURL();
-            logger.info("** Looking for server with all system defaults");
+            logger.info("** Looking for server on [{}]", engineURL);
+            pingServer(defaults, engineURL, false);
             defaults.setApiKey(null);
 
             String version = getVersion(engineURL, defaults.getApiKey());
@@ -54,7 +57,7 @@ public class Configure {
             if (version != null) {
                 logger.info("** Success!! All configured default settings are working! Talking to AbEngine version {}", version);
                 if (reconfigure) {
-                    Boolean yes = getBooleanValue("Do you still wish to reconfigure your settings? (n)", "n");
+                    Boolean yes = getBooleanValue("Reconfigure? ", "Do you still wish to reconfigure your settings? (n)", "n");
                     if (yes) {
                         configure(file, defaults, engineURL);
                     }
@@ -70,9 +73,23 @@ public class Configure {
         if (ns.getBoolean("test")) {
             testConfig(defaults);
         } else
-            logger.info("{}", defaults);
+            logger.info("** Success! Login name [{}], apiKey [{}]", defaults.getDefaultUser(), defaults.getApiKey());
 
 
+    }
+
+    private static void configureLogger(Boolean debug) {
+        Logger.getRootLogger().getLoggerRepository().resetConfiguration();
+        ConsoleAppender console = new ConsoleAppender();
+        String PATTERN = "%m%n";
+        console.setLayout(new PatternLayout(PATTERN));
+        console.setThreshold( (debug?Level.TRACE:Level.INFO));
+        console.activateOptions();
+
+        //add appender to any Logger (here is root)
+        Logger.getRootLogger().addAppender(console);
+
+        logger = LoggerFactory.getLogger(Configure.class);
     }
 
     static File getFile(String configFile, Namespace ns) {
@@ -104,6 +121,12 @@ public class Configure {
                 .help("Test the stored configuration")
                 .action(Arguments.storeTrue());
 
+        parser.addArgument("-x", "--debug")
+                .required(false)
+                .setDefault(false)
+                .help("Debug level logging")
+                .action(Arguments.storeTrue());
+
         parser.addArgument("-c", "--path")
                 .setDefault("./conf")
                 .required(false)
@@ -112,10 +135,6 @@ public class Configure {
         parser.addArgument("-u", "--user")
                 .required(false)
                 .help("User authorised to create registrations");
-
-        parser.addArgument("-p", "--pass")
-                .required(false)
-                .help("Password for --user");
 
         Namespace ns = null;
         try {
@@ -128,52 +147,45 @@ public class Configure {
     }
 
     private static boolean configure(File file, ConfigProperties defaults, String engineURL) {
-        String pingResult;
         String version;
 
-        pingResult = pingServer(engineURL, null, null);
+        internalPass = getPassword();
 
-
-        while (!pingResult.equalsIgnoreCase("pong!") && !pingResult.equalsIgnoreCase("auth")) {
-            logger.error("!! Unable to ping AbEngine on [{}]", engineURL);
-            engineURL = getValue("** Enter AbEngine URL ({})", engineURL);
-            if (engineURL != null) {
-                pingResult = pingServer(engineURL, null, null);
-            }
-            defaults.setEngineURL(engineURL);
-        }
-        logger.info("** Success in connecting to AuditBucket on {}", engineURL);
+        engineURL = pingServer(defaults, engineURL, true); // We've already pinged.
         //Boolean resetUser = getBooleanValue("** Have you configured AuditBucket with an alternative security domain?", "N");
         String apiKey = defaults.getApiKey();
 
         version = getVersion(engineURL, apiKey);
         String user = defaults.getDefaultUser();
         String company = defaults.getCompany();
+
         while (version == null) {
 
             do {
-                String message = "** Enter a unique login name to register ";
-                if ( user!=null )
-                    message = message + "default = ({})";
-                user = getValue(message, user);
-            } while (user == null );
-            do{
-                String message="** Enter the company name for the login ["+user+"] ";
-                if ( company != null )
-                    message = message + "default = ({})";
-                company = getValue(message, company);
-            } while (company == null );
+                String message = "** Enter a unique user name to register. ";
+                String prompt = "User name to register ";
+                if (user != null)
+                    prompt = prompt+ "("+ user+")";
+                user = getValue(prompt+":", message, user);
+            } while (user == null);
+            do {
+                String message = "** Enter the company name for the user ["+user+"]. ";
+                String prompt = "Company name to register ";
+                if (company != null)
+                    prompt = prompt+ "("+ company+")";
+                company = getValue(prompt +":",message, company);
+            } while (company == null);
 
             SystemUserResultBean suResult = registerUser(engineURL, internalUser, internalPass, user, company);
 
             if (suResult != null) {
                 version = getVersion(engineURL, suResult.getApiKey());
             } else {
-                logger.info("Unable to register the system user with the login [{}]", internalUser);
+                logger.info("Unable to register the user name with the admin account [{}]", internalUser);
                 System.exit(-1);
             }
-            if ( !suResult.getCompanyName().equalsIgnoreCase(company)){
-                logger.error ("The Login name [{}] is already in use with another company. Login names must be unique. Try using an email address", user);
+            if (!suResult.getCompanyName().equalsIgnoreCase(company)) {
+                logger.error("The Login name [{}] is already in use with another company. Login names must be unique. Try using an email address", user);
                 System.exit(-1);
             }
             if (version != null) {
@@ -184,12 +196,28 @@ public class Configure {
         }
 
         writeConfiguration(file, defaults);
-        logger.info("** Success!! Configured settings are working.");
         return true;
     }
 
-    private static Boolean getBooleanValue(String message, String def) {
-        String yn = getValue(message, def);
+    private static String pingServer(ConfigProperties defaults, String engineURL, boolean suppressSuccessMessage) {
+        String pingResult;
+        pingResult = pingServer(engineURL, null, null);
+
+        while (!pingResult.equalsIgnoreCase("pong!") && !pingResult.equalsIgnoreCase("auth")) {
+            logger.error("!! Unable to ping AbEngine on [{}]", engineURL);
+            engineURL = getValue("URL: ", "** Enter AbEngine URL ({})", engineURL);
+            if (engineURL != null) {
+                pingResult = pingServer(engineURL, null, null);
+            }
+            defaults.setEngineURL(engineURL);
+        }
+        if ( !suppressSuccessMessage)
+            logger.info("** Success in connecting to AuditBucket on {}", engineURL);
+        return engineURL;
+    }
+
+    private static Boolean getBooleanValue(String prompt, String message, String def) {
+        String yn = getValue(prompt, message, def);
         if (yn.toLowerCase().startsWith("y"))
             yn = "true";
         else if (yn.toLowerCase().startsWith("n"))
@@ -200,7 +228,7 @@ public class Configure {
     private static void testConfig(ConfigProperties defaults) {
         String version = getVersion(defaults.getEngineURL(), defaults.getApiKey());
         if (version == null)
-            logger.error("** Error communicating with AuditBucket using parameters {}", defaults);
+            logger.error("!! Error communicating with AuditBucket using parameters {}", defaults);
         else
             logger.info("** Success communicating with AuditBucket {} with parameters {}", version, defaults);
     }
@@ -213,7 +241,7 @@ public class Configure {
             //ObjectMapper om = new ObjectMapper();
 
             //om.writerWithDefaultPrettyPrinter().writeValue(file, defaults);
-            logger.info("** Configuration defaults written to {} ", file.getAbsoluteFile().toString());
+            logger.debug("** Configuration defaults written to {} ", file.getAbsoluteFile().toString());
         } catch (IOException e) {
             logger.error("Unexpected", e);
         }
@@ -254,23 +282,41 @@ public class Configure {
         return result.get("ab-engine.version").toString();
     }
 
-    private static String getValue(String message, String defaultValue) {
-        if (defaultValue != null)
-            logger.info(message, defaultValue);
-        else
-            logger.info(message);
+    private static String getPassword() {
+        Console console = System.console();
+        if (console != null) {
+            // read password into the char array
+            char[] pwd = console.readPassword("Password for "+internalUser+": ");
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String value = null;
-        try {
-            value = br.readLine();
-            if (value != null)
-                if (value.equals("") || value.equals(defaultValue))
-                    return defaultValue;
-        } catch (IOException ioe) {
-            System.out.println("IO error trying to read your name!");
-            System.exit(1);
+            // prints
+            return String.copyValueOf(pwd);
+        } else {
+            logger.error ("Could not get Console");
         }
-        return value;
+
+        return null;
+    }
+
+    private static String getValue(String prompt, String message, String defaultValue) {
+        Console console = System.console();
+        if (console != null) {
+            // read password into the char array
+            if ( defaultValue != null)
+                logger.info(message, defaultValue);
+            else
+                logger.info(message);
+
+            String result = console.readLine(prompt);
+            if ( result == null || result.equals(""))
+                return defaultValue;
+            else
+                return result;
+
+        } else {
+            logger.error ("Could not get Console");
+            System.exit(2);
+        }
+        return defaultValue;
+
     }
 }
