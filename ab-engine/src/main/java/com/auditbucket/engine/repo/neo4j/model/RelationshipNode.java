f154ef99cd751e4dc7a3aff76b69a6c62d5cc80c
Mike Holdsworth <mike@monowai.com>
2014-06-16 19:57:57 +1200

    DAT-99 - Endpoints /query/relationships implemented

---
+++ ab-engine/src/main/java/com/auditbucket/engine/repo/neo4j/model/RelationshipNode.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright (c) 2012-2014 "Monowai Developments Limited"
+ *
+ * This file is part of AuditBucket.
+ *
+ * AuditBucket is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * AuditBucket is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with AuditBucket.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.auditbucket.engine.repo.neo4j.model;
+
+import com.auditbucket.registration.model.Relationship;
+import org.neo4j.graphdb.Direction;
+import org.springframework.data.annotation.TypeAlias;
+import org.springframework.data.neo4j.annotation.GraphId;
+import org.springframework.data.neo4j.annotation.NodeEntity;
+import org.springframework.data.neo4j.annotation.RelatedTo;
+
+/**
+ * User: mike
+ * Date: 16/06/14
+ * Time: 2:33 PM
+ */
+@NodeEntity
+@TypeAlias("Relationship")
+public class RelationshipNode implements Relationship {
+    @GraphId
+    Long id;
+
+    @RelatedTo(type="KNOWN_RELATIONSHIP", direction = Direction.INCOMING)
+    ConceptNode concept;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ConceptNode getConcept() {
+        return concept;
+    }
+
+    public void setConcept(ConceptNode concept) {
+        this.concept = concept;
+    }
+
+    private String name;
+}
