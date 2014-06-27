ee35c6c6f3880b7b3f2a74d125d1eaa7b0dc2b36
SathishK <sathish.hari@gmail.com>
2014-06-27 17:09:51 +0530

    Integrate Stormpath via Spring security, enable login endpoint

---
+++ ab-engine/src/main/java/com/auditbucket/authentication/model/User.java
@@ -0,0 +1,78 @@
+package com.auditbucket.authentication.model;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+public class User implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 8908570723211178151L;
+
+	private String userId;
+	private String userName;
+	private String userEmail;
+	private String status;
+	private List<String> userRoles = new ArrayList<String>();
+	private String apiKeyId;
+	private String apiKey;
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getUserEmail() {
+		return userEmail;
+	}
+
+	public void setUserEmail(String userEmail) {
+		this.userEmail = userEmail;
+	}
+
+	public String getApiKeyId() {
+		return apiKeyId;
+	}
+
+	public void setApiKeyId(String apiKeyId) {
+		this.apiKeyId = apiKeyId;
+	}
+
+	public String getApiKey() {
+		return apiKey;
+	}
+
+	public void setApiKey(String apiKey) {
+		this.apiKey = apiKey;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public void addUserRole(String role) {
+		userRoles.add(role);
+	}
+
+	public Object[] getUserRoles() {
+		return this.userRoles.toArray();
+	}
+
+}
