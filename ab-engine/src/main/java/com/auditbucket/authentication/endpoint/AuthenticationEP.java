f9e890324dc44caa5f3736c8ce604b8c747d9729
SathishK <sathish.hari@gmail.com>
2014-06-29 02:43:13 +0530

    Fix to retrieve API Key from Systemuser Service

--- ab-engine/src/main/java/com/auditbucket/authentication/endpoint/AuthenticationEP.java
+++ ab-engine/src/main/java/com/auditbucket/authentication/endpoint/AuthenticationEP.java
@@ -24,36 +24,36 @@ import com.auditbucket.authentication.service.UserProfileService;
 
 @Controller
 public class AuthenticationEP {
-	private static final Logger logger = LoggerFactory
-			.getLogger(AuthenticationEP.class);
+    private static final Logger logger = LoggerFactory
+            .getLogger(AuthenticationEP.class);
 
-	@Autowired(required = false)
-	@Qualifier("authenticationManager")
-	AuthenticationManager authenticationManager;
+    @Autowired(required = false)
+    @Qualifier("authenticationManager")
+    AuthenticationManager authenticationManager;
 
     @Autowired
     private UserProfileService userProfileService;
 
     @RequestMapping(value = "/login", consumes = MediaType.APPLICATION_JSON_VALUE, method = RequestMethod.POST)
-	@ResponseBody
-	public ResponseEntity<User> handleLogin(@RequestBody LoginRequest loginRequest) throws Exception {
-		String username = loginRequest.getUsername();
-		String password = loginRequest.getPassword();
-		
-		UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(
-				username, password);
-		try {
-			Authentication auth = authenticationManager.authenticate(token);
-			SecurityContextHolder.getContext().setAuthentication(auth);
-			User user = userProfileService.getUser(auth);
-			
-			logger.info("User profile - " + user);
-			
-			return new ResponseEntity<User>(user, HttpStatus.OK);
-		} catch (AuthenticationException e) {
-			logger.error("Error authenticating User", e);
-			return new ResponseEntity<User>(HttpStatus.UNAUTHORIZED);
-		}
-	}
+    @ResponseBody
+    public ResponseEntity<User> handleLogin(@RequestBody LoginRequest loginRequest) throws Exception {
+        String username = loginRequest.getUsername();
+        String password = loginRequest.getPassword();
+
+        UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(
+                username, password);
+        try {
+            Authentication auth = authenticationManager.authenticate(token);
+            SecurityContextHolder.getContext().setAuthentication(auth);
+            User user = userProfileService.getUser(auth);
+
+            logger.info("User profile - " + user);
+
+            return new ResponseEntity<User>(user, HttpStatus.OK);
+        } catch (AuthenticationException e) {
+            logger.error("Error authenticating User", e);
+            return new ResponseEntity<User>(HttpStatus.UNAUTHORIZED);
+        }
+    }
 
 }
