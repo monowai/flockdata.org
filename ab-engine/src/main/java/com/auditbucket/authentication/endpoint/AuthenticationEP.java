07eab21cdf3d2d417aa7b2f23e05c2783ae4f795
SathishK <sathish.hari@gmail.com>
2014-06-29 13:38:43 +0530

    Refactor AuthenticationEP, extract new interface for user profile service

--- ab-engine/src/main/java/com/auditbucket/authentication/endpoint/AuthenticationEP.java
+++ ab-engine/src/main/java/com/auditbucket/authentication/endpoint/AuthenticationEP.java
@@ -1,5 +1,7 @@
 package com.auditbucket.authentication.endpoint;
 
+import java.util.Iterator;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
