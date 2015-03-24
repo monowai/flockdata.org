568dad741c468112f61a707e87b49cf7d7eee2d2
Mike Holdsworth <mike@monowai.com>
2015-03-24 15:02:21 +1300

    DAT-352 - GeoData optimization

--- fd-engine/src/main/java/org/flockdata/geography/GeoEvaluator.java
+++ fd-engine/src/main/java/org/flockdata/geography/GeoEvaluator.java
@@ -17,7 +17,7 @@
  * along with FlockData.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-package org.flockdata.engine.track;
+package org.flockdata.geography;
 
 import org.flockdata.registration.model.Tag;
 import org.neo4j.graphdb.DynamicLabel;
