2261b63279210369af85250a620d6cf76ac05b3f
Mike Holdsworth <mike@monowai.com>
2015-05-19 10:45:13 +1200

    merging branches

--- fd-common/src/main/java/org/flockdata/transform/csv/CsvEntityMapper.java
+++ fd-common/src/main/java/org/flockdata/transform/csv/CsvEntityMapper.java
@@ -19,6 +19,7 @@
 
 package org.flockdata.transform.csv;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import org.flockdata.helper.FlockException;
 import org.flockdata.profile.ImportProfile;
 import org.flockdata.profile.model.ProfileConfiguration;
