29dd306f5948704532c6ce6538a4b70a7bb9bd60
Mike Holdsworth <mike@monowai.com>
2015-06-15 20:38:12 +1200

    DAT-464 - KV Integration channels

--- fd-store/src/main/java/org/flockdata/kv/service/KvManager.java
+++ fd-store/src/main/java/org/flockdata/kv/service/KvManager.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012-2014 "FlockData LLC"
+ * Copyright (c) 2012-2015 "FlockData LLC"
  *
  * This file is part of FlockData.
  *
@@ -147,12 +147,6 @@ public class KvManager implements KvService {
             logger.debug("Sync write begins {}", kvBean);
             doKvWrite(kvBean);
         }
-    }
-
-
-    void doKvWriteAsync(KvContentBean kvBean) {
-        kvGateway.doKvWrite(kvBean);
-        //return new AsyncResult<>(null);
     }
 
     /**
