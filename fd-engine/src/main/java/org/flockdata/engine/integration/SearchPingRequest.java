94e62cc6fd725e66e94f17ffeafaa86e9e40b23c
I <mike@monowai.com>
2016-02-14 15:46:43 +1300

    DAT-550 DSL migration. Search Ping

--- fd-engine/src/main/java/org/flockdata/engine/integration/SearchPingRequest.java
+++ fd-engine/src/main/java/org/flockdata/engine/integration/SearchPingRequest.java
@@ -1,9 +1,8 @@
 package org.flockdata.engine.integration;
 
 /**
- *
  * For SDN4 Un-managed Extensions
- *
+ * <p/>
  * Created by mike on 21/07/15.
  */
 
@@ -14,11 +13,16 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpMethod;
+import org.springframework.integration.annotation.Gateway;
 import org.springframework.integration.annotation.IntegrationComponentScan;
+import org.springframework.integration.annotation.MessagingGateway;
+import org.springframework.integration.channel.DirectChannel;
 import org.springframework.integration.dsl.IntegrationFlow;
 import org.springframework.integration.dsl.IntegrationFlows;
 import org.springframework.integration.http.outbound.HttpRequestExecutingMessageHandler;
+import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.MessageHandler;
+import org.springframework.messaging.handler.annotation.Payload;
 
 /**
  * Created by mike on 3/07/15.
@@ -26,12 +30,24 @@ import org.springframework.messaging.MessageHandler;
 
 @Configuration
 @IntegrationComponentScan
-@Profile({"integration","production"})
+@Profile({"integration", "production"})
 public class SearchPingRequest {
 
     @Autowired
     @Qualifier("engineConfig")
     PlatformConfig engineConfig;
+
+    @Bean
+    MessageChannel fdSearchPing(){
+        return new DirectChannel();
+    }
+
+    @MessagingGateway
+    public interface MonitoringGateway {
+        @Payload("new java.util.Date()")
+        @Gateway(requestChannel = "fdSearchPing")
+        String ping();
+    }
 
     @Bean
     IntegrationFlow doFdSearchPing() {
@@ -43,13 +59,12 @@ public class SearchPingRequest {
 
     private MessageHandler fdPingRequest() {
         HttpRequestExecutingMessageHandler handler =
-                new HttpRequestExecutingMessageHandler(engineConfig.getFdSearch()+ "/v1/admin/ping");
+                new HttpRequestExecutingMessageHandler(engineConfig.getFdSearch() + "/v1/admin/ping");
         handler.setExpectedResponseType(String.class);
         handler.setHttpMethod(HttpMethod.GET);
 
         return handler;
     }
-
 
 
 }
