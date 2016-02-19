dcef8ab4dc628b87ea91e44c6a50d536b49646a2
I <mike@monowai.com>
2016-02-19 21:20:19 +1300

    DAT-550 fd-store config and minor refactor

--- fd-store/src/main/java/org/flockdata/store/endpoints/DataEP.java
+++ fd-store/src/main/java/org/flockdata/store/endpoints/DataEP.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("${fd-store.system.api:api}/v1/data")
-public class ContentEP {
+public class DataEP {
 
 
 }
---
+++ fd-store/src/main/java/org/flockdata/store/service/StoreService.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2012-2014 "FlockData LLC"
+ *
+ * This file is part of FlockData.
+ *
+ * FlockData is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * FlockData is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with FlockData.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.flockdata.store.service;
+
+import org.flockdata.helper.FlockException;
+import org.flockdata.model.Entity;
+import org.flockdata.model.Log;
+import org.flockdata.store.KvContent;
+import org.flockdata.store.LogRequest;
+import org.flockdata.store.bean.KvContentBean;
+import org.flockdata.track.bean.TrackResultBean;
+
+import java.io.IOException;
+
+/**
+ * User: mike
+ * Date: 6/09/14
+ * Time: 12:07 PM
+ */
+public interface StoreService {
+    String ping();
+
+    void purge(String indexName);
+
+    Log prepareLog(TrackResultBean content, Log log) throws IOException;
+
+    KvContent getContent(LogRequest logRequest);
+
+    void delete(Entity entity, Log change);
+
+    void doWrite( KvContentBean kvBean) throws FlockException;
+
+}
