96a591ac4fc14ee4287291733f0f44c90865388f
I <mike@monowai.com>
2016-02-25 10:41:32 +1300

    DAT-552 key refactor

--- fd-integration-tests/src/test/java/org/flockdata/test/integration/TestEngineDeploys.java
+++ fd-integration-tests/src/test/java/org/flockdata/test/integration/TestEngineDeploys.java
@@ -8,18 +8,19 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.boot.test.WebIntegrationTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 /**
  * Created by mike on 21/02/16.
  */
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(FdEngine.class)
 // fd-engine port
-@WebIntegrationTest({"server.port=9090", "management.port=9070"})
+//@WebIntegrationTest({"server.port=9090", "management.port=9070"})
+@SpringApplicationConfiguration(FdEngine.class)
 @ActiveProfiles("integration")
+@TestPropertySource("/application.yml")
 public class TestEngineDeploys {
 
     @Autowired
@@ -28,7 +29,10 @@ public class TestEngineDeploys {
     private static Logger logger = LoggerFactory.getLogger(TestEngineDeploys.class);
     @Test
     public void testNothing(){
+        // The aim is to have fd-search deployed so that it can be queried
         logger.info("Hello world");
 
     }
+
+
 }
