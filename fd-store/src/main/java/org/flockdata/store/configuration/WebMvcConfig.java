a46d7d7e788325a05db69557610829ff24514377
Mike Holdsworth <mike@flockdata.com>
2016-02-27 12:09:49 +1300

    Default page for /api

--- fd-store/src/main/java/org/flockdata/store/configuration/WebMvcConfig.java
+++ fd-store/src/main/java/org/flockdata/store/configuration/WebMvcConfig.java
@@ -1,3 +1,11 @@
+/*
+ * Copyright (c) 2016. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+ * Morbi non lorem porttitor neque feugiat blandit. Ut vitae ipsum eget quam lacinia accumsan.
+ * Etiam sed turpis ac ipsum condimentum fringilla. Maecenas magna.
+ * Proin dapibus sapien vel ante. Aliquam erat volutpat. Pellentesque sagittis ligula eget metus.
+ * Vestibulum commodo. Ut rhoncus gravida arcu.
+ */
+
 package org.flockdata.store.configuration;
 
 import org.springframework.context.annotation.Configuration;
@@ -18,6 +26,11 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter {
     @RequestMapping("/")
     String home() {
         return "index.html";
+    }
+
+    @RequestMapping("/api")
+    String api () {
+        return home();
     }
 
 }
