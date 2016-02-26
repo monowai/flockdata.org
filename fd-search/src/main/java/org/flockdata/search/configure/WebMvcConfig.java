31c5e4d97401b1bf247ede601662a4391fb554c5
I <mike@monowai.com>
2016-02-27 12:09:49 +1300

    Default page for /api

--- fd-search/src/main/java/org/flockdata/search/configure/WebMvcConfig.java
+++ fd-search/src/main/java/org/flockdata/search/configure/WebMvcConfig.java
@@ -1,3 +1,11 @@
+/*
+ * Copyright (c) 2016. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+ * Morbi non lorem porttitor neque feugiat blandit. Ut vitae ipsum eget quam lacinia accumsan.
+ * Etiam sed turpis ac ipsum condimentum fringilla. Maecenas magna.
+ * Proin dapibus sapien vel ante. Aliquam erat volutpat. Pellentesque sagittis ligula eget metus.
+ * Vestibulum commodo. Ut rhoncus gravida arcu.
+ */
+
 package org.flockdata.search.configure;
 
 import org.springframework.context.annotation.Configuration;
@@ -17,6 +25,11 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter {
     @RequestMapping("/")
     String home() {
         return "index.html";
+    }
+
+    @RequestMapping("/api")
+    String api () {
+        return home();
     }
 
 }
