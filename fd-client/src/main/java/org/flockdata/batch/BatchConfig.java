910c316b35b49d7bc4fdaf49c558c1f37ac06714
Mike Holdsworth <mike@flockdata.com>
2016-03-13 13:56:02 +1300

    Docker and configuration changes - merge from fd-batch

--- fd-client/src/main/java/org/flockdata/batch/BatchConfig.java
+++ fd-client/src/main/java/org/flockdata/batch/BatchConfig.java
@@ -24,10 +24,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.annotation.PropertySources;
+import org.springframework.context.annotation.*;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
 import java.io.FileInputStream;
@@ -54,6 +51,7 @@ import java.util.Map;
         @PropertySource(value = "classpath:/fd-batch.properties"),
         @PropertySource(value = "file:${org.fd.batch.properties}", ignoreResourceNotFound = true)
 })
+@Profile("fd-batch")
 public class BatchConfig {
     private Logger logger = LoggerFactory.getLogger(BatchConfig.class);
 
