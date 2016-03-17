630df927c5dd8d65a7c5c52535d30ad28afab05d
Mike Holdsworth <mike@flockdata.com>
2016-03-17 16:15:30 +1300

    LiveReload port problem

--- fd-view/Gruntfile.js
+++ fd-view/Gruntfile.js
@@ -57,7 +57,7 @@ module.exports = function (grunt) {
         files: ['<%= yeoman.app %>/scripts/{,*/}*.js'],
         tasks: ['newer:jshint:all'],
         options: {
-          livereload: true
+          livereload: '<%= connect.options.livereload %>'
         }
       },
       jsTest: {
@@ -88,12 +88,12 @@ module.exports = function (grunt) {
       options: {
         port: 9000,
         // Change this to '0.0.0.0' to access the server from outside.
-        hostname: 'localhost',
-        livereload: 35729
+        hostname: 'localhost'
       },
       livereload: {
         options: {
           open: 'http://localhost:9000',
+          livereload: 66771,
           base: [
             '.tmp',
             '<%= yeoman.app %>'
