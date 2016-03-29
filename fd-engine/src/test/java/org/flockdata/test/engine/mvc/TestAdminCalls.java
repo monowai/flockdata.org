e9e13e6d35c0f91d78320e19fa3b165c661c7022
I <mike@monowai.com>
2016-03-29 19:14:06 +1300

    Test

--- fd-engine/src/test/java/org/flockdata/test/engine/mvc/TestAdminCalls.java
+++ fd-engine/src/test/java/org/flockdata/test/engine/mvc/TestAdminCalls.java
@@ -78,7 +78,7 @@ public class TestAdminCalls extends MvcBase {
         if (results.get("fd-search").toString().equalsIgnoreCase("ok"))
             logger.warn("fd-search is running in a not unit test fashion....");
         else
-            assertTrue(results.get("fd-search").toString().contains("Problem"));
+            assertTrue(results.get("fd-search").toString(), results.get("fd-search").toString().contains("Problem"));
 
         // No api key, auth only DAT-203
         results = getHealth(mike());
