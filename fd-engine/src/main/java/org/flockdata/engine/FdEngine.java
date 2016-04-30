7cc3100f048c6ab4369179192b6e4c6d579d1548
Mike Holdsworth <mike@monowai.com>
2016-05-01 11:40:39 +1200

    DAT-606 SpringCloud library upgrades. Various Spring library upgrades

--- fd-engine/src/main/java/org/flockdata/engine/FdEngine.java
+++ fd-engine/src/main/java/org/flockdata/engine/FdEngine.java
@@ -24,10 +24,10 @@ import org.flockdata.track.service.SchemaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 import javax.annotation.PostConstruct;
+
+//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * Java configuration of fd-engines graphWriting servicee
@@ -43,8 +43,7 @@ import javax.annotation.PostConstruct;
                 "org.flockdata.engine", "org.flockdata.geography",
                 "org.flockdata.authentication", "org.flockdata.shared"})
 
-@EnableConfigurationProperties()
-@EnableDiscoveryClient()
+//@EnableDiscoveryClient
 public class FdEngine {
     public static void main(String[] args) {
         SpringApplication.run(FdEngine.class, args);
