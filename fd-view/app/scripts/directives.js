e5c541ea35c6f5fba90744ded0ac1b26a320b4e9
dmty <dmtyak@gmail.com>
2016-05-19 14:20:27 +1200

    DAT-601 Fix bower fix autofocus

--- fd-view/app/scripts/directives.js
+++ fd-view/app/scripts/directives.js
@@ -129,6 +129,16 @@ angular.module('fdView.directives', [])
       templateUrl: 'views/partials/singleLogPopup.html'
     }
   })
+  .directive('autofocus', ['$timeout', function($timeout) {
+    return {
+      restrict: 'A',
+      link : function($scope, $element) {
+        $timeout(function() {
+          $element[0].focus();
+        });
+      }
+    }
+  }])
   .directive('ngHeight', ['$window', function ($window) {
     return {
         restrict: 'A',
